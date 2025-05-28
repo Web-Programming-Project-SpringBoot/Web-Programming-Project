20fe9a5ec99c2f95710e09537991ed52009d3bcd
ozkanyllmaz <ozkanyilmaz.dev@gmail.com>
2025-05-28 21:40:36 +0300

    Complete Data Layer: Implement CRUD operations and fix lazy loading issues

--- src/main/java/com/example/taskmanagement/controller/UserController.java
+++ src/main/java/com/example/taskmanagement/controller/UserController.java
@@ -1,11 +1,17 @@
 package com.example.taskmanagement.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.taskmanagement.entity.User;
@@ -23,9 +29,38 @@ public class UserController {
 		return userService.createUser(user);
 	}
 	
-	@GetMapping("/{userName}")
-	public User findByUserName(@PathVariable String userName) {
-		return userService.findByUserName(userName);
-	}
+	@GetMapping("/{username}")
+    public ResponseEntity<User> getUserByUsername(@PathVariable String username) {
+        User user = userService.findByUsername(username);
+        return ResponseEntity.ok(user);
+    }
+	
+	@GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+	
+    @GetMapping("/id/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.getUserById(id);
+        return ResponseEntity.ok(user);
+    }
+	
+	@PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id,
+    		@RequestParam String userName,
+    		@RequestParam String email,
+    		@RequestParam String password,
+    		@RequestParam boolean enable) {
+        User updatedUser = userService.updateUser(id, userName, email, password, enable);
+        return ResponseEntity.ok(updatedUser);
+    }
+	
+	@DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
